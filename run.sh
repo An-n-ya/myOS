db1a68231ff94eef5b4f096fd70c8b0d130e9ca5
ankh04 <ankh04@icloud.com>
2022-11-08 17:27:32 +0800

    bash文件更新“

--- run.sh
+++ run.sh
@@ -1,9 +1,23 @@
-nasm -I include/ -o mbr.bin mbr.s
+if [ ! -d "./out" ]; then
+    # 如果out不存在就创建该文件夹
+    mkdir out
+fi
 
-nasm -I include/ -o loader.bin loader2.s
+if [ -e "hd60M.img" ]; then
+    echo "123"
+    rm -rf hd60M.img
+fi
 
-dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+# 编译
+nasm -I include/ -o ./out/mbr.bin mbr.s
 
-dd if=loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
+nasm -I include/ -o ./out/loader.bin loader2.s
+
+# 创建hd60M.img镜像文件
+bximage -q -func=create -hd=60M -imgmode=flat hd60M.img
+
+dd if=./out/mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
+
+dd if=./out/loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
 bochs -f bochsrc.disk
