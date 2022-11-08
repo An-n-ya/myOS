65b060889fba331c7aa19f82d41225f402f77bde
ankh04 <ankh04@icloud.com>
2022-11-08 16:59:32 +0800

    bug修改， 显示p

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 nasm -I include/ -o mbr.bin mbr.s
 
-nasm -I include/ -o loader.bin loader.s
+nasm -I include/ -o loader.bin loader2.s
 
 dd if=mbr.bin of=hd60M.img bs=512 count=1 conv=notrunc
 
-dd if=loader.bin of=hd60M.img bs=512 count=1 seek=2 conv=notrunc
+dd if=loader.bin of=hd60M.img bs=512 count=4 seek=2 conv=notrunc
 
 bochs -f bochsrc.disk
