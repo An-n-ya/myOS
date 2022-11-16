d41c4db3b1a5b1820238e2c3a799a603d47701fb
ankh04 <ankh04@icloud.com>
2022-11-16 15:40:39 +0800

    使用c语言设置中断函数

--- kernel/interrupt.c
+++ kernel/interrupt.c
@@ -24,6 +24,9 @@ struct gate_desc
 static void make_idt_desc(struct gate_desc *p_gdesc, uint8_t attr, intr_handler function);
 static struct gate_desc idt[IDT_DESC_CNT]; // 中断门描述符数组
 
+char *intr_name[IDT_DESC_CNT];        // 用于保存异常的名字
+intr_handler idt_table[IDT_DESC_CNT]; // 定义中断函数的数组
+
 extern intr_handler intr_entry_table[IDT_DESC_CNT]; // kernel.s 中定义的intr_entry_table
 
 // 配置8259A
@@ -67,11 +70,53 @@ static void idt_desc_init(void)
     put_str("   idt_desc_init done\n");
 }
 
+// 中断处理函数， 参数是中断向量号
+static void general_intr_handler(uint8_t vec_nr)
+{
+    if (vec_nr == 0x27 | vec_nr == 0x2f)
+    {
+        return;
+    }
+    put_str("int vector : 0x");
+    put_int(vec_nr);
+    put_char('\n');
+}
+
+static void exception_init(void)
+{
+    int i;
+    for (i = 0; i < IDT_DESC_CNT; i++)
+    {
+        idt_table[i] = general_intr_handler;
+        intr_name[i] = "unknown"; // 先统一赋值为 unknown
+    }
+    intr_name[0] = "#DE Divide Error";
+    intr_name[1] = "#DB Debug Exception";
+    intr_name[2] = "NMI Interrupt";
+    intr_name[3] = "#BP Breakpoint Exception";
+    intr_name[4] = "#OF Overflow Exception";
+    intr_name[5] = "#BR BOUND Range Exceeded Exception";
+    intr_name[6] = "#UD Invalid Opcode Exception";
+    intr_name[7] = "#NM Device Not Available Exception";
+    intr_name[8] = "#DF Double Fault Exception";
+    intr_name[9] = "Coprocessor Segment Overrun";
+    intr_name[10] = "#TS Invalid TSS Exception";
+    intr_name[11] = "#NP Segment Not Present";
+    intr_name[12] = "#SS Stack Fault Exception";
+    intr_name[13] = "#GP General Protection Exception";
+    intr_name[14] = "#PF Page-Fault Exception";
+    intr_name[16] = "#MF 0x87 FPU Floating-Point Error";
+    intr_name[17] = "#AC Alignment Check Exception";
+    intr_name[18] = "#MC Machine-Check Exception";
+    intr_name[19] = "#XF SIMD Floating-Point Exception";
+}
+
 void idt_init()
 {
     put_str("idt_init start\n");
     idt_desc_init(); // 初始化中断描述符
-    pic_init();      // 初始化8259A
+    exception_init();
+    pic_init(); // 初始化8259A
 
     // 加载idt
     // 把idt地址变成一个48位的
--- lib/kernel/print.s
+++ lib/kernel/print.s
@@ -1,138 +1,194 @@
-TI_GDT          equ 0
-RPL0            equ 0
-SELECTOR_VIDEO  equ (0x0003<<3) + TI_GDT + RPL0
+; 内核打印功能实现
+TI_GDT equ 0
+RPL0 equ 0
+SELECTOR_VIDEO equ (0x0003 << 3) + TI_GDT + RPL0
 
+section .data
+put_int_buffer dd 0, 0
 
 [bits 32]
 section .text
-;---------------------- put_char -------------------
-; 把栈中的1个字符写入光标所在处
-;---------------------------------------------------
+; put_char，将栈中的一个字符写入光标所在处
 global put_char
-put_char:
-    pushad                      ;备份所有32位的寄存器
-    mov     ax, SELECTOR_VIDEO
-    mov     gs, ax              ; 把视频段放入gs段寄存器
-
-    ; 获取光标位置
-    ; 先获取高8位
-    mov     dx, 0x03d4
-    mov     al, 0x0e
-    out     dx, al
-    mov     dx, 0x03d5
-    in      al, dx
-    mov     ah, al
-    ; 再获取低8位
-    mov     dx, 0x03d4
-    mov     al, 0x0f
-    out     dx, al
-    mov     dx, 0x03d5
-    in      al, dx
-
-    ; 将光标存入bx
-    mov     bx, ax
-    mov     ecx,[esp + 36]      ; 获取栈中第一个字符（由于之前保存了寄存器，占用了32字节，因此需要+36）h
-
-    cmp     cl, 0xd             ; 对CR和LF做特殊处理
-    jz      .is_carriage_return
-    cmp     cl, 0xa
-    jz      .is_line_feed
-
-    cmp     cl, 0x8             ; 对退格键做处理
-    jz      .is_backspace
-    jmp     .put_other
-
-    .is_backspace:
-        dec     bx
-        shl     bx,             1
-        mov     byte [gs:bx],   0x20
-        inc     bx,
-        mov     byte [gs:bx],   0x07
-        shr     bx,             1
-        jmp     .set_cursor
-
-    .put_other:
-        shl     bx,             1
-        mov     [gs:bx],        cl
-        inc     bx
-        mov     byte [gs:bx],   0x07
-        shr     bx,             1
-        inc     bx
-        cmp     bx,             2000    ;判断是否需要滚屏
-        jl      .set_cursor
-
-    .is_line_feed:
-    .is_carriage_return:
-        xor     dx,         dx          ; dx清零，是被除数的高16为
-        mov     ax,         bx          ; ax是被除数的低16位
-        mov     si,         80          ; 除数
-        div     si
-        sub     bx,         dx          ; 光标位置 - 光标位置除以80的余数
-                                        ; 即取整, 移动到行首
-    .is_carriage_return_end:
-        add     bx,         80
-        cmp     bx,         2000
-    .is_line_feed_end:
-        jl      .set_cursor
-
-    .roll_screen:
-        cld
-        mov     ecx,        960         ; 一共有 2000 - 80 = 1920个字符要搬运，共2840字节，一次搬运四字节
-        mov     esi,        0xc00b80a0  ; 第一行行首
-        mov     edi,        0xc00b8000  ; 第零行行首
-        rep     movsd
-
-        ;;;; 将最后一行填充为空白
-        mov     ebx,        3840
-        mov     ecx,        80
-    .cls:
-        mov  word [gs:ebx], 0x0720      ; 黑底白字的空格
-        add     ebx,        2
-        loop    .cls
-        mov     bx,         1920        ; 将光标重置为最后一行行首
-    
-    ; 将光标设置为bx
-    .set_cursor:
-        mov     dx,         0x03d4
-        mov     al,         0x0e
-        out     dx,         al
-        mov     dx,         0x03d5
-        mov     al,         bh          ; 设置bx的高8位
-        out     dx,         al
-
-        mov     dx,         0x03d4
-        mov     al,         0x0f
-        out     dx,         al
-        mov     dx,         0x03d5
-        mov     al,         bl          ; 设置bx的低8位
-        out     dx,         al
-    .put_char_done:
-        popad
-        ret
-
-;---------------- put_str --------------------------
-;put_str 通过put_char来打印以0字符结尾的字符串
-;---------------------------------------------------
 global put_str
+global put_int
+
+put_int:
+    pushad
+    mov ebp, esp
+    mov eax, [ebp + 4 * 9]
+    mov edx, eax
+    mov edi, 7
+    mov ecx, 8
+    mov ebx, put_int_buffer
+
+.16based_4bits:
+    and edx, 0x0000000F
+    cmp edx, 9
+    jg .is_A2F
+    add edx, '0'
+    jmp .store
+
+.is_A2F:
+    sub edx, 10
+    add edx, 'A'
+
+.store:
+    mov [ebx + edi], dl
+    dec edi
+    shr eax, 4
+    mov edx, eax
+    loop .16based_4bits
+
+.ready_print:
+    inc edi
+
+.skip_prefix_0:
+    cmp edi, 8
+    je .full0
+
+.go_on_skip:
+    mov cl, [put_int_buffer + edi]
+    inc edi
+    cmp cl, '0'
+    je .skip_prefix_0
+    dec edi
+    jmp .put_each_num
+
+.full0:
+    mov cl, '0'
+.put_each_num:
+    push ecx
+    call put_char
+    add esp, 4
+    inc edi
+    mov cl, [put_int_buffer + edi]
+    cmp edi, 8
+    jl .put_each_num
+    popad
+    ret
+
+; 字符串打印函数，基于put_char封装
 put_str:
-    ; 备份ebx 和 ecx两个寄存器
-    push    ebx
-    push    ecx
-    xor     ecx,    ecx             ; 清空ecx
-    mov     ebx,    [esp + 12]      ; 获取待打印的字符串地址
-    .go_on:
-        mov     cl,     [ebx]
-        cmp     cl,     0           ; 如果遇到 \0 就结束打印
-        jz      .str_over
-        push    ecx                 ; 保存ecx
-        call    put_char            ; 打印字符
-        add     esp,    4           ; 回收栈的空间，更新栈顶
-        inc     ebx                 ; 使得光标后移 
-        jmp     .go_on
-    .str_over:
-        pop     ecx
-        pop     ebx                 ; 还原ecx和ebx
-        ret
+    push ebx
+    push ecx
+    xor ecx, ecx
+    mov ebx, [esp + 12]
 
+.go_on:
+    mov cl, [ebx]
+    cmp cl, 0
+    jz .str_over
+    push ecx
+    call put_char
+    add esp, 4
+    inc ebx
+    jmp .go_on
 
+.str_over:
+    pop ecx
+    pop ebx
+    ret
 
+put_char:
+    pushad
+    mov ax, SELECTOR_VIDEO
+    mov gs, ax
+
+    ; 获取当前光标位置
+    mov dx, 0x03d4
+    mov al, 0x0e
+    out dx, al
+    mov dx, 0x03d5
+    in al, dx
+    mov ah, al
+
+    mov dx, 0x03d4
+    mov al, 0x0f
+    out dx, al
+    mov dx, 0x03d5
+    in al, dx
+
+    mov bx, ax
+    mov ecx, [esp + 36]
+
+    cmp cl, 0xd
+    jz .is_carriage_return
+    cmp cl, 0xa
+    jz .is_line_feed
+
+    cmp cl, 0x8
+    jz .is_backspace
+    jmp .put_other
+
+.is_backspace:
+    dec bx
+    shl bx, 1
+
+    mov byte [gs:bx], 0x20
+    inc bx
+    mov byte [gs:bx], 0x07
+    shr bx, 1
+    jmp .set_cursor
+
+.put_other:
+    shl bx, 1
+    mov [gs:bx], cl
+    inc bx
+    mov byte [gs:bx], 0x07
+    shr bx, 1
+    inc bx
+    cmp bx, 2000
+    jl .set_cursor
+
+.is_line_feed:
+.is_carriage_return:
+    xor dx, dx
+    mov ax, bx
+    mov si, 80
+
+    div si
+
+    sub bx, dx
+
+.is_carriage_return_end:
+    add bx, 80
+    cmp bx, 2000
+.is_line_feed_end:
+    jl .set_cursor
+
+.roll_screeen:
+    cld 
+    mov ecx, 960
+
+    mov esi, 0xc00b80a0
+    mov edi, 0xc00b8000
+    rep movsd
+
+    mov ebx, 3840
+    mov ecx, 80
+
+.cls:
+    mov word [gs:ebx], 0x0720
+    add ebx, 2
+    loop .cls
+    mov bx, 1920
+
+.set_cursor:
+    mov dx, 0x03d4
+    mov al, 0x0e
+    out dx, al
+    mov dx, 0x03d5
+    mov al, bh
+    out dx, al
+
+    mov dx, 0x03d4
+    mov al, 0x0f
+    out dx, al
+    mov dx, 0x03d5
+    mov al, bl
+    out dx, al
+
+.put_char_done:
+    popad
+    ret
